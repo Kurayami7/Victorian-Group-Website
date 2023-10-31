f42c278f07a481d7c215124495d618e734512ca3
Areaf <darkgulliotine7@gmail.com>
2023-10-31 18:03:58 -0400

    Enhanced styles via borders

---
+++ about.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>The Victorian Group - About Us </title>
+  <link rel="shortcut icon" href="images/favicon.jfif" type="image/x-icon" />
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/utilities.css">
+  <link rel="stylesheet" href="css/responsive.css">
+  <script src="javascript/script.js" defer></script> <!-- Download script while parsing html -->
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                <img src="images/logo.png" alt="Victorian Telescope Logo">
+                <!-- width="150px" -->
+            </div>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+            <div class="search">
+                <input type="text" name="search-box" id="search-box">
+                <button class="btn" id="btn">Search</button>
+            </div>
+        </nav>
+    </header>
+    <main>
+
+    <div class="cards aboutCards">
+		<section class="aboutSection">
+			<h1 class="aboutHeading">About Us</h1>
+      <img src="images/about.jpg" alt="Victorian Group dream builing" class="aboutImg">
+			<p>At The Victorian Group, we are passionate about <em>preserving the history and beauty of classic telescopes.</em> We believe that the craftsmanship and attention to detail in vintage telescopes are unmatchable by modern mass-produced instruments.</p>
+
+			<p>Our collection of vintage telescopes has been carefully <em></em>curated</em> to ensure that each piece is in excellent condition and ready for use. We offer telescopes from a variety of eras, including Victorian, Edwardian, and Art Deco.</p>
+
+      
+			<p>In addition to our <em>telescopes</em>, we also offer a range of accessories and replacement parts to help you maintain your  <em>vintage instrument</em>. We are committed to providing our customers with the highest quality products and customer service.</p>
+
+      
+			<p>Thank you for choosing  <em>The Victorian Group</em>.</p>
+		</section>
+	</main>
+
+	<footer class="flex-all-center">
+        <p>Copyright &copy; The Victorian Group. All Rights Reserved.</p>
+  </footer>
+</body>
+</html>
+
+
